9e1c2c9c1204a1dce9366f10807aebf70267491c
sank8dalvi <sanket.dalvi.ssd@gmail.com>
2018-10-06 01:57:11 +0530

    Initial commit

---
+++ Sampletests.c
@@ -0,0 +1,21 @@
+//#include <graphics.h>
+//
+//main()
+//{
+//   int gd = DETECT, gm, c , x = 100, y = 50;
+//
+//   initgraph(&gd, &gm, "C:\\TC\\BGI");
+//
+//   for ( c = 0 ; c < 5 ; c++ )
+//   {
+//       setlinestyle(1, 0, 2);
+//
+//       line(x, y, x+200, y);
+//       y = y + 25;
+//   }
+//
+//   getch();
+//   closegraph();
+//   return 0;
+//}
+
